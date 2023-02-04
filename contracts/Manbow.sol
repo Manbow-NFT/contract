3026f89eab8faf8cd7c4940b34466a9205f39ff7
banr1 <banri.yanahama@gmail.com>
2023-02-04 14:59:59 +0900

    add reproduce and burn functions

--- contracts/Manbow.sol
+++ contracts/Manbow.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC1155Drop.sol";
 
-contract Contract is ERC1155Drop {
+contract Manbow is ERC1155Drop {
     constructor(
         string memory _name,
         string memory _symbol,
@@ -19,4 +19,16 @@ contract Contract is ERC1155Drop {
             _primarySaleRecipient
         )
     {}
+
+    function reproduce(
+        address _to,
+        uint256 _amount,
+        bytes memory _data
+    ) external onlyOwner {
+        _mint(_to, 0, _amount, _data);
+    }
+
+    function burn(address _to, uint256 _amount) external onlyOwner {
+        _burn(_to, 0, _amount);
+    }
 }
