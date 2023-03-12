1c2db34ad479256dde7a490f677ce77f39f36985
banr1 <banri.yanahama@gmail.com>
2023-03-12 17:34:01 +0900

    add token rate and copy claim function that will be updated to merge function

--- contracts/Manbow.sol
+++ contracts/Manbow.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import "@thirdweb-dev/contracts/base/ERC1155Drop.sol";
 
 contract Manbow is ERC1155Drop {
+    mapping(uint256 => mapping(uint256 => uint256)) public tokenRate;
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -18,5 +20,41 @@ contract Manbow is ERC1155Drop {
             _royaltyBps,
             _primarySaleRecipient
         )
-    {}
+    {
+        tokenRate[0][2] = 20;
+        tokenRate[0][4] = 100;
+        tokenRate[2][4] = 5;
+    }
+
+    function claim(
+        address _receiver,
+        uint256 _tokenId,
+        uint256 _quantity,
+        address _currency,
+        uint256 _pricePerToken,
+        AllowlistProof calldata _allowlistProof,
+        bytes memory _data
+    ) public payable virtual override {
+        _beforeClaim(_tokenId, _receiver, _quantity, _currency, _pricePerToken, _allowlistProof, _data);
+
+        ClaimCondition memory condition = claimCondition[_tokenId];
+        bytes32 activeConditionId = conditionId[_tokenId];
+
+        verifyClaim(_tokenId, _dropMsgSender(), _quantity, _currency, _pricePerToken, _allowlistProof);
+
+        // Update contract state.
+        condition.supplyClaimed += _quantity;
+        supplyClaimedByWallet[activeConditionId][_dropMsgSender()] += _quantity;
+        claimCondition[_tokenId] = condition;
+
+        // If there's a price, collect price.
+        _collectPriceOnClaim(address(0), _quantity, _currency, _pricePerToken);
+
+        // Mint the relevant NFTs to claimer.
+        _transferTokensOnClaim(_receiver, _tokenId, _quantity);
+
+        emit TokensClaimed(_dropMsgSender(), _receiver, _tokenId, _quantity);
+
+        _afterClaim(_tokenId, _receiver, _quantity, _currency, _pricePerToken, _allowlistProof, _data);
+    }
 }
